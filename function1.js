b892b260b33f465015e13ae1482a18fcfb48f57b
AdityaSingh-web18 <adityasingh180710@gmail.com>
2024-06-27 12:00:14 +0530

    question

---
+++ function1.js
@@ -0,0 +1,13 @@
+//aman add two numbers send to naman.naman square it send it to rohit.rohit divide it by 10.
+function add(a, b) {
+  return a + b;
+}
+let aman = add(5, 1);
+function square(x) {
+  return x ** 2;
+}
+let naman = square(aman);
+function divide(y) {
+  return y / 10;
+}
+console.log(divide(naman));
